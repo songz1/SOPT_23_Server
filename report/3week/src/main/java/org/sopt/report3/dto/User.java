a14a2b5d93ffc99d5abe2b3806948f07cc986458
songz1 <21pori30@gmail.com>
2018-11-30 14:38:09 +0900

    report3

--- report/3week/src/main/java/org/sopt/report3/dto/User.java
+++ report/3week/src/main/java/org/sopt/report3/dto/User.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
     // 회원 고유번호
-    private int user_idx;
+    private int user_idx = 0;
     // 회원 이름
     private String name = "";
     // 회원 파트
